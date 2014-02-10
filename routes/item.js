f48649b060643936b9531cabf5d04203bcc6e1dc
jdsmith99 <jdsmith99@hotmail.com>
2014-02-10 15:45:38 -0600

    updated item.update method

--- routes/item.js
+++ routes/item.js
@@ -38,6 +38,41 @@ Item.prototype = {
 			});
 	},
 
+	update: function (req, res) {
+
+		var parmItem = req.body;
+		var parmId = parmItem._id
+		var newItem = item(parmItem);
+
+		item.findById (parmId, function (err, item) {
+			if (err) {
+				res.send(500, err)
+				return;
+			}
+
+			if (newItem.description != null) item.description = newItem.description;
+			if (newItem.code != null) item.code = newItem.code;
+			if (newItem.cost != null) item.cost = newItem.cost;
+			if (newItem.weight != null) item.weight = newItem.weight;
+			if (newItem.calories != null) item.calories = newItem.calories;
+			if (newItem.glutenFree != null)	item.glutenFree = newItem.glutenFree;
+			if (newItem.lowSugar != null) item.lowSugar = newItem.lowSugar;
+			if (newItem.nutFree != null) item.nutFree = newItem.nutFree;
+			if (newItem.highFiber != null) item.highFiber = newItem.highFiber;
+			if (newItem.organic != null) item.organic = newItem.organic;
+			if (newItem.kosher != null) item.kosher = newItem.kosher;
+			if (newItem.vegan != null) item.vegan = newItem.vegan;
+			item.save ( function (err, updatedItem){
+				if (err) {
+					res.send(500, err)
+					return;
+				}
+
+				res.send(200, updatedItem);
+			});
+		});
+	},
+
 	activate: function (req, res) {
 		var id = req.params.id;
 		item.update ({_id : id}, {active : true}, {multi : false}, function (err, numAffected) {
